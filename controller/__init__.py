ce8fea1ac6bb3441012a3b724cf31b492389068d
saniya <saniyaelzabeth1543@gmail.com>
2025-05-15 09:12:56 +0530

    final update

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,4 +1,3 @@
 from . import main
 from . import school_portal
 from . import school_event_snippet
-from . import website_sale
--- controller/school_portal.py
+++ controller/school_portal.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-from os.path import exists
-from unicodedata import category
-from addons.website_sale.controllers.main import WebsiteSale
-from openpyxl.worksheet import page
-from requests import post
-
 from odoo import http
 from odoo.http import request
 
@@ -134,26 +128,3 @@ class CustomWebsite(http.Controller):
         return request.render('school.web_event_template', {
             'event': events
         })
-
-# class WebsiteSale(WebsiteSale):
-#     print("helloooo")
-#     @http.route(['/shops'], type='http', auth="user", website=True, sitemap=True)
-#     def shop(self, page=0, category=None, search='', **post):
-#         print("hiiii")
-#         result = super().shop(page=page, category=category, search=search, **post)
-#
-#         user = request.env.user
-#         partner = user.partner_id
-#
-#         domain = []
-#         if partner.product_ids:
-#             domain = [('id', 'in', partner.product_ids.ids)]
-#             print('hello')
-#         elif partner.product_category_ids:
-#             domain = [('product_category_ids', 'in', partner.product_category_ids.ids)]
-#
-#         if domain:
-#             filtered_products = request.env['product.template'].search(domain)
-#             result.qcontext['products'] = filtered_products
-#
-#         return result
